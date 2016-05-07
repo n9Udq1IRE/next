8b86adf3579b7df182b8095ec6dd5c80f5a27428
QAMUGYSOL <nixiwafic.qamugysol@unimedia.fr>
2016-05-07 14:24:36 +0200

    Create Grunfile.js

---
+++ Grunfile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['uglify']);
+
+};
