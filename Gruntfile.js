c139828dc2a618e5c030d06f859b9830df0cd4ba
QAMUGYSOL <nixiwafic.qamugysol@unimedia.fr>
2016-05-07 16:12:07 +0200

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,10 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        src: ['projets/radios/assets/js/*.js'],
-        dest: 'dist/<%= pkg.name %>.js'
-      }
-    },
     uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
       dist: {
         files: {
-          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+          'projets/radios/assets/js/application.min.js': ['projets/radios/assets/js/application.js']
         }
       }
     }
